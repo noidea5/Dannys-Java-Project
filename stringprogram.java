25f666e5a11854662f67991a31068d33ec70a21b
zootsuitproductions <dannysantanasf@gmail.com>
2018-03-18 23:46:23 -0700

    Update stringprogram.java

--- stringprogram.java
+++ stringprogram.java
@@ -3,7 +3,7 @@ public class stringprogram {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Please write a sentence: ");
+		System.out.println("Please write a few sentences: ");
 		String sentences = reader.nextLine();
 		String[] sentenceArray = sentences.split("\\. ");
 		//separates the inputed string for each period and space
@@ -16,10 +16,6 @@ public class stringprogram {
 				finalOutput += ". ";
 			//since the period and space are removed, they must be added in (except for the last sentence, since it is not split)
 		}
-		String newString = sentences.substring(0, 1).toUpperCase() + sentences.substring(1);
 		System.out.println(finalOutput);
 	}
-
 }
-
-
